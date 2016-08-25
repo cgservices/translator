13bf9aade2a415fdb9eb14651aaa6e81de1cd272
Sander van Rossum <sander.van.rossumg@cg.nl>
2016-08-25 15:31:14 +0200

    Added search filters

--- app/controllers/translator/translations_controller.rb
+++ app/controllers/translator/translations_controller.rb
@@ -3,35 +3,21 @@ module Translator
     before_filter :auth
     layout Translator.layout_name
 
-    def index
+    def translator_keys
       # section = params[:key].present? && params[:key] + '.'
       params[:group] = "all"
       # @sections = Translator.keys_for_strings(:group => params[:group]).map {|k| k = k.scan(/^[a-zA-Z0-9\-_]*\./)[0]; k ? k.gsub('.', '') : false}.select{|k| k}.uniq.sort
       # @groups = ["framework", "application", "deleted"]
-      @keys = Translator.keys_for_strings(:group => params[:group])
-      if params[:key]
-        @keys = @keys.select {|k| k.include?(params[:key]) }
-      end
+      Translator.keys_for_strings(:group => params[:group])
+    end
 
-      if params[:search]
-        @keys = @keys.select {|k|
-          Translator.locales.any? {|locale| I18n.translate("#{k}", :locale => locale).to_s.downcase.include?(params[:search].downcase) || k.to_s.downcase.include?(params[:search].downcase)}
-        }
-      end
-      if params[:translated] == '1'
-        @keys = @keys.select {|k|
-          Translator.locales.all? {|locale| (begin I18n.backend.translate(locale, "#{k}") rescue nil; end).present? }
-        }
-      end
+    def keys
+      @keys = filter(translator_keys)
+    end
 
-      if params[:translated] == '0'
-        @keys = @keys.select {|k|
-          Translator.locales.any? {|locale| (begin I18n.backend.translate(locale, "#{k}") rescue nil; end).blank? }
-        }
-      end
-
+    def index
+      @keys = filter(translator_keys)
       @keys = paginate(@keys)
-
     end
 
     def create
@@ -59,6 +45,43 @@ module Translator
       @page = 1 if @page == 0
       @total_pages = (collection.count / 25.0).ceil
       collection[(@page-1)*25..@page*25]
+    end
+
+    def filter(collection)
+      if params[:key_exclude].present?
+        collection = collection.select { |k| !params[:key_exclude].split.any? { |s| k.include?(s) } }
+      end
+
+      if params[:key]
+        collection = collection.select { |k| k.include?(params[:key]) }
+      end
+
+      if params[:search]
+        collection = collection.select { |k|
+          Translator.locales.any? { |locale| I18n.translate("#{k}", :locale => locale).to_s.downcase.include?(params[:search].downcase) || k.to_s.downcase.include?(params[:search].downcase)}
+        }
+      end
+
+      if params[:translated] == '2'
+        collection = collection.select { |k|
+          translations = []
+          Translator.locales.each { |locale| translations << I18n.backend.translate(locale, "#{k}") }
+          translations.uniq.length == 1
+        }
+      end
+
+      if params[:translated] == '1'
+        collection = collection.select { |k|
+          Translator.locales.all? { |locale| (begin I18n.backend.translate(locale, "#{k}") rescue nil; end).present? }
+        }
+      end
+
+      if params[:translated] == '0'
+        collection = collection.select { |k|
+          Translator.locales.any? { |locale| (begin I18n.backend.translate(locale, "#{k}") rescue nil; end).blank? }
+        }
+      end
+      collection
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # When Rails >= 3.1
 if defined?(Translator::Engine)
   Translator::Engine.routes.draw do
-    resources :translations
+    resources :translations, only: [:index, :create, :destroy]
+    get '/translations/keys', to: "translations#keys"
   end
 else
   Rails.application.routes.draw do
-    resources :translations, :to => "Translator::Translations"
+    resources :translations, :to => "Translator::Translations", only: [:index, :create, :destroy]
+    get '/translations/keys', to: "translations#keys"
   end
 end
